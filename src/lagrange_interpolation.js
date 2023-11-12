6d40585b742f486bdf0127824226b7bbd0249dfa
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-11-13 02:54:34 +0300

    jquery removed, react scripts are loaded on page ready because content is nonow an included script and is located at the bottom of the layout

--- src/lagrange_interpolation.js
+++ src/lagrange_interpolation.js
@@ -1,10 +1,9 @@
-import jQuery from "jquery";
 import React, { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
 
-export function main() {
+function main() {
   const root = createRoot(document.getElementById('react-root'));
   root.render(<h1>Hello, world!</h1>);
 }
 
-jQuery(document).ready(main);
+main()
