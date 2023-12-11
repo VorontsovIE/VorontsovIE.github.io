b8508d9e86c6b9bbadb1f85ebd456cdf2fdc4244
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-12-12 01:32:47 +0300

    an attempt to make responsive component

--- src/lagrange_interpolation.js
+++ src/lagrange_interpolation.js
@@ -1,49 +1,82 @@
-import React, { StrictMode } from "react";
+import React, { StrictMode, useState, useRef } from "react";
 import { createRoot } from 'react-dom/client';
+import styles from './lagrange_interpolation.css';
 
-import * as d3 from "d3";
-import {useRef, useEffect} from "react";
+import {scaleLinear, line, curveMonotoneX} from "d3";
+// import {useRef, useEffect} from "react";
 import { MathComponent } from "mathjax-react";
+import { v4 as uuidv4 } from 'uuid';
 
-window.d3 = d3;
-
-function line(f) {
-  let scaleX = d3.scaleLinear().range([0, 300]).domain([0, 13]);
-  let scaleY = d3.scaleLinear().range([150, 0]).domain([-1, 1]);
-  let points = scaleX.ticks(100).map(x => [x, f(x)]).map(([x,y]) => [scaleX(x), scaleY(y)]);
-  let pathGenerator = d3.line().curve(d3.curveMonotoneX);
-  return pathGenerator(points);
+function Point({x, y, r=4}) {
+  return <circle cx={x} cy={y} r={r} />
 }
 
 function Curve({path}) {
   return (
-    <path d={path} fill="none" stroke="steelblue" stroke-width={1.5} />
+    <path d={path} fill="none" stroke="steelblue" strokeWidth={1.5} />
   );
 }
 
+function pathLine(f) {
+  let scaleX = scaleLinear().range([0, 800]).domain([0, 13]);
+  let scaleY = scaleLinear().range([500, 0]).domain([-1, 1]);
+  let points = scaleX.ticks(100).map(x => [x, f(x)]).map(([x,y]) => [scaleX(x), scaleY(y)]);
+  let pathGenerator = line().curve(curveMonotoneX);
+  return pathGenerator(points);
+}
+
 function SvgSample() {
+  const [points, setPoints] = useState([]);
+
+  const svgRef = useRef(null);
+  const svgLeft = () => svgRef.current.getBoundingClientRect().left + window.scrollX;
+  const svgTop  = () => svgRef.current.getBoundingClientRect().top  + window.scrollY;
+
+  let addPoint = function (event) {
+    const coordX = Math.round(event.pageX - svgLeft());
+    const coordY = Math.round(event.pageY - svgTop());
+    setPoints([
+      ...points,
+      {
+        x: coordX,
+        y: coordY,
+        id: uuidv4(),
+      }
+    ]);
+  }
+
   return (
-    <svg style={{
-      border: "2px solid gold"
-    }}>
-      <Curve path={line(x=>Math.sin(x))} />
+    <svg ref={svgRef} className={styles.svgCanvas} onClick={event => addPoint(event)}>
+      <Curve path={pathLine(x=>Math.sin(x))} />
+      {points.map( point => <Point key={point.id} {...point} /> )}
     </svg>
   );
 }
 
 function LagrangeInterpolationPage(){
+  let [formula, setFormula] = useState(String.raw`6.02 \cdot 10^{23}`);
+  let changeFormula = () => setFormula(String.raw`x^2+2x+1=0`);
   return (
     <>
     <h1>Интерполяционный многочлен Лагранжа</h1>
-    <h2>Полиномиальная интерполяция</h2>
+    <h2 onClick={changeFormula}>Полиномиальная интерполяция</h2>
     <SvgSample />
-    <p>Avogadro's constant is <MathComponent display={false} tex={String.raw`6.02 \cdot 10^{23}`} /></p>
+    <p>Formula is <MathComponent display={false} tex={formula} /></p>
     </>
   );
 }
+
+function App() {
+  return (
+    <StrictMode>
+      <LagrangeInterpolationPage />
+    </StrictMode>
+  );
+}
+
 function main() {
   const root = createRoot(document.getElementById('react-root'));
-  root.render(<LagrangeInterpolationPage />);
+  root.render(<App />);
 }
 
 main()
