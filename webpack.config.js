4239b92fd792154472d7a1c65cff5d10229c1b6f
Ilya Vorontsov <vorontsov.i.e@gmail.com>
2023-12-12 01:30:03 +0300

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,24 @@
 const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // cache: false,
   entry: {
     main: ['./src/main.js'],
-    lagrange_interpolation: ['./src/lagrange_interpolation.js'],
+    'lagrange-interpolation': ['./src/lagrange_interpolation.js'],
   },
+  // plugins: [
+  //   new HtmlWebpackPlugin({filename: '_site/[name]/index.html', }),
+  // ],
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js'
+    filename: '[name].js',
+    // filename: '[name].[contenthash].js',
   },
+  optimization: {
+    usedExports: true,
+  },
+  // mode: "development",
   module: {
     rules: [
       {
@@ -17,10 +27,25 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ['@babel/preset-env', "@babel/preset-react"],
           }
         }
-      }
-    ]
+      },
+      // {
+      //   test: /\.tsx?$/,
+      //   use: 'ts-loader',
+      //   exclude: /node_modules/,
+      // },
+      {
+        test: /\.css$/i,
+        use: [
+          "style-loader",
+          {loader: "css-loader", options: {modules: true}},
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx', '.css'],
   },
 };
